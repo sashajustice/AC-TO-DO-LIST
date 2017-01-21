a8fe8dcc7d80bf0aaf92a101662605fa3e708948
mikeadossi <mikeadossi@gmail.com>
2017-01-20 17:19:15 -0800

    add isCompleted feature in db & started work on editing task

--- database/schema.sql
+++ database/schema.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS projects;
 CREATE TABLE projects (
   id SERIAL PRIMARY KEY,
   project_name VARCHAR(255) NOT NULL,
-  description TEXT NOT NULL
+  description TEXT NOT NULL,
+  isCompleted BOOLEAN DEFAULT FALSE
 );
